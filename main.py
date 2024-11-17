5e6bd590ed6fa413c146c9adc920517f2d031a38
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-11-17 14:52:44 +0100

    Intento de complete robot detection

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 from src.aruco_detection.board_detection import detect_aruco_board
 from src.aruco_detection.lateral_board_detection import detect_lateral_aruco_board
+from src.aruco_detection.robot_board_detection import detect_complete_aruco_board
 from src.utils.Calibracion import calibracion
 
 # Configuración de la cámara
@@ -13,6 +14,7 @@ def main():
     # Llama a la función de detección de tablero ArUco
     ##detect_aruco_board(camMatrix,distCoeffs)
     detect_lateral_aruco_board(camMatrix, distCoeffs)
+    ##detect_complete_aruco_board(camMatrix, distCoeffs)
 
 if __name__ == "__main__":
     main()
